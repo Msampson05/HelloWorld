adab5a5472b83baa223712d21117ee4db923efed
Megan Sampson <msampson@nyc.yearup.org>
2022-10-18 09:26:55 -0400

    Learning to code

--- hello.js
+++ hello.js
@@ -1,7 +1,26 @@
 // Hello World 
+console.log("Hello Class")
 
-"use strict";
+var firstName = "Megan";
+var lastName= "Sampson";
 
-var message = "Hello World"
-console.log(message);
+// var firstname = lastname + "," + firstname;
+//console.log(fullname);
 
+console.log(lastName + "," +firstName);
+
+var num1 = 56;
+var num2 = 20;
+var answer1 = num1 + num2;
+var answer2 = num1 - num2;
+var answer3 = num1 * num2;
+var answer4 = num1 / num2;
+var answer5 = num1 % num2;
+var answer6 = -num1;
+
+
+
+console.log(2 ** 3);
+
+var answer6 = (Math.pow(2,3));
+console.log(answer6);
